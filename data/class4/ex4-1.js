0bc31ffab9a3665d9945ff7dd10687d632e72b42
Adebayo Oduwobi <adebayooduwobi@gmail.com>
2020-12-15 17:04:41 -0600

    adds closeLists

--- data/class4/ex4-1.js
+++ data/class4/ex4-1.js
@@ -19,13 +19,56 @@ const getUsers = () => {
   })
 }
 
+const getUsersTodos = (userId) => {
+  return new Promise((resolve, reject) => {
+    const userRequest = new XMLHttpRequest();
+
+    userRequest.addEventListener('readystatechange', () => {
+      if(userRequest.readyState === 4 && userRequest.status ==200) {
+        resolve(JSON.parse(userRequest.responseText));
+      } else if(userRequest.readyState === 4) {
+        reject('something went wrong');
+      }
+    });
+
+  userRequest.open('GET', `https://jsonplaceholder.typicode.com/todos?userId=${userId}`);
+    userRequest.send();
+  })
+}
+
 const createUserList = userData => {
   userData.forEach(user => {
-    userListElement.insertAdjacentElement('beforeend', 
+    userListElement.insertAdjacentHTML('beforeend', 
     `<li data-user-id = ${user.id}>${user.name}</li>`)
   });
+}
+const createTodoList = todos => {
+  const todoList = document.createElement('UL');
+  todoList.classList.add('open');
+  todos.forEach(todo => {
+    todoList.insertAdjacentHTML('beforeend', `<li>${todo.title}</li>`)
+  })
+  return todoList
 }
 getUsers()
 .then(userData => {
   createUserList(userData);
 });
+
+const closeLists = () => {
+  document.querySelectorAll('.open').forEach(element => {
+    element.innerHTML = '';
+    element.classList.remove('open');
+  });
+}
+userListElement.addEventListener('click', e => {
+  if (e.target.nodeName === 'LI') {
+    const currentUserElement = e.target
+    getUsersTodos(currentUserElement.dataset.userId)
+    .then(todos => createTodoList(todos))
+    .then(todoList => {
+      currentUserElement.appendChild(todoList);
+    })
+  }
+
+})
